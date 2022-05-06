ac22d90005f6565a8e973df7d9e4acf5ee5bc7b8
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2022-05-06 14:36:26 +0200

    Add blured background when modal is showed

--- src/components/shared/modal/Modal.tsx
+++ src/components/shared/modal/Modal.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { ModalContext } from '@/utils/context/ModalContext';
 import { useOnClickAway } from 'use-on-click-away';
+import classNames from 'classnames';
 
 const Modal = () => {
   const { closeModal, isModalShowed, modalContent } = useContext(ModalContext);
@@ -21,20 +22,28 @@ const Modal = () => {
     closeModal();
   });
 
+  const modalBackgroundClassnames = classNames({
+    'modal-background': true,
+    'is-modal-background-showed': isModalShowed,
+  });
+
   return (
-    <CSSTransition
-      in={isModalShowed}
-      timeout={300}
-      classNames='modal'
-      unmountOnExit
-    >
-      <div ref={ref}>
-        {modalContent}
-        <div className='modal__button-wrapper'>
-          <button onClick={closeModal}>Close modal</button>
+    <>
+      <CSSTransition
+        in={isModalShowed}
+        timeout={300}
+        classNames='modal'
+        unmountOnExit
+      >
+        <div ref={ref}>
+          {modalContent}
+          <div className='modal__button-wrapper'>
+            <button onClick={closeModal}>Close modal</button>
+          </div>
         </div>
-      </div>
-    </CSSTransition>
+      </CSSTransition>
+      <div className={modalBackgroundClassnames} />
+    </>
   );
 };
 
