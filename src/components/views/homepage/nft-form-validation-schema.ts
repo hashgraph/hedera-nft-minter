a813bc68f4fc39bb8e09cff85a8448ede83e0b97
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2022-05-12 11:16:00 +0200

    Update validation schema

--- src/components/views/homepage/nft-form-validation-schema.ts
+++ src/components/views/homepage/nft-form-validation-schema.ts
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 export const ValidationSchema = yup.object().shape({
   image: yup.mixed().test('type', 'Only image files are accepted!', (value) => {
-    return value ? (value.type.includes('image/')) : true
-}),
+    return value ? value.type.includes('image/') : true;
+  }),
   name: yup
     .string()
     .min(3, 'Too Short!')
@@ -14,22 +14,10 @@ export const ValidationSchema = yup.object().shape({
     .min(3, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Required'),
-  creator: yup
-    .string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!'),
-  creatorDID: yup
-    .string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!'),
-  description: yup
-    .string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!'),
-  qty: yup.number()
-    .min(1, 'Min 1!')
-    .max(10, 'Max 10!')
-    .required('Required'),
+  creator: yup.string().min(3, 'Too Short!').max(50, 'Too Long!'),
+  creatorDID: yup.string().min(3, 'Too Short!').max(50, 'Too Long!'),
+  description: yup.string().min(3, 'Too Short!').max(50, 'Too Long!'),
+  qty: yup.number().min(1, 'Min 1!').max(10, 'Max 10!').required('Required'),
   properties: yup.array().of(
     yup.object().shape({
       name: yup
@@ -59,75 +47,40 @@ export const ValidationSchema = yup.object().shape({
     })
   ),
 
-  treasury: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
+  treasury: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  kyc: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  admin: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  freeze: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  wipe: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  supply: yup.mixed().oneOf(['no', 'account', 'custom']).required('Required'),
+  treasury_account_id: yup.string().when('treasury', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  kyc_key: yup.string().when('kyc', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  admin_key: yup.string().when('admin', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  freeze_key: yup.string().when('freeze', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  wipe_key: yup.string().when('wipe', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  pause_key: yup.string().when('pause', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
+  supply_key: yup.string().when('supply', {
+    is: 'custom',
+    then: (schema) => schema.required('Required'),
+  }),
 
-  kyc: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
-
-  admin: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
-
-  freeze: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
-
-  wipe: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
-
-  supply: yup
-    .mixed()
-    .oneOf(['no', 'account', 'custom'])
-    .required('Required')
-  ,
-
-  treasury_account_id: yup.string()
-    .when('treasury', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  kyc_key: yup.string()
-    .when('kyc', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  admin_key: yup.string()
-    .when('admin', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  freeze_key: yup.string()
-    .when('freeze', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  wipe_key: yup.string()
-    .when('wipe', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  pause_key: yup.string()
-    .when('pause', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
-  supply_key: yup.string()
-    .when('supply', {
-      is: 'custom',
-      then: (schema) => schema.required('Required')
-    }),
+  fees: yup.mixed().oneOf(['royaltyFee', 'fractionalFee', 'fixedFee']),
 });
