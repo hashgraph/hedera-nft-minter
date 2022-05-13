b49cfe9f1a6b75bc384f9664d700fde4bbba59f5
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2022-05-13 15:35:02 +0200

    Refactor nft-form-fees

--- src/components/views/homepage/nft-form-fees.tsx
+++ src/components/views/homepage/nft-form-fees.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FieldArray, Field } from 'formik';
 import FieldWrapper from '@/components/shared/form/FieldWrapper';
 import Switch from './../../shared/form/switch/Switch';
@@ -9,6 +9,138 @@ type Props = {
 };
 
 const NftFormFees = ({ fees }: Props) => {
+  const renderRoyaltyFeeFormFields = useCallback(
+    () => (
+      <div className='form__row__fees'>
+        <label htmlFor='null'>Royalty Fee:</label>
+
+        <FieldWrapper
+          name={'activeFees.royaltyFee.feeCollectorAccountId'}
+          type='text'
+          label='Fee collector account ID'
+        />
+
+        <FieldWrapper
+          name={'activeFees.royaltyFee.fallbackFee'}
+          type='number'
+          label='Fallback fee'
+        />
+
+        <FieldWrapper
+          name={'activeFees.royaltyFee.numerator'}
+          type='number'
+          label='Numerator'
+        />
+
+        <FieldWrapper
+          name={'activeFees.royaltyFee.denominator'}
+          type='number'
+          label='Denominator'
+        />
+      </div>
+    ),
+    []
+  );
+
+  const renderFractionalFeeFormFields = useCallback(
+    () => (
+      <div className='form__row__fees'>
+        <label htmlFor='null'>Fractional Fee:</label>
+
+        <FieldWrapper
+          name={'activeFees.fractionalFee.feeCollectorAccountId'}
+          type='text'
+          label='Fee collector account ID'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fractionalFee.numerator'}
+          type='number'
+          label='Numerator'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fractionalFee.denominator'}
+          type='number'
+          label='Denominator'
+        />
+        <FieldWrapper
+          name={'activeFees.fractionalFee.min'}
+          type='number'
+          label='Min'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fractionalFee.max'}
+          type='number'
+          label='Max'
+        />
+
+        <Switch
+          name={'activeFees.fractionalFee.assessmentMethod'}
+          options={[
+            { name: 'Inclusive', value: 'inclusive' },
+            { name: 'Exclusive', value: 'exclusive' },
+          ]}
+        />
+      </div>
+    ),
+    []
+  );
+
+  const renderFixedFeeFormFields = useCallback(
+    () => (
+      <div className='form__row__fees'>
+        <label htmlFor='null'>Fixed Fee:</label>
+
+        <FieldWrapper
+          name={'activeFees.fixedFee.feeCollectorAccountId'}
+          type='text'
+          label='Fee collector account ID'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fixedFee.denominatingTokenId'}
+          type='text'
+          label='Denominating token ID'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fixedFee.amount'}
+          type='number'
+          label='Amount'
+        />
+
+        <FieldWrapper
+          name={'activeFees.fixedFee.hbarAmount'}
+          type='number'
+          label='HBar amount'
+        />
+      </div>
+    ),
+    []
+  );
+
+  const renderCheckedFeesFormFields = useCallback(() => {
+    fees?.map((fee: string) => {
+      switch (fee) {
+        case 'royaltyFee':
+          return renderRoyaltyFeeFormFields();
+        case 'fractionalFee':
+          return renderFractionalFeeFormFields();
+        case 'fixedFee':
+          return renderFixedFeeFormFields();
+        default:
+          return;
+      }
+    });
+  }, [
+    fees,
+    renderRoyaltyFeeFormFields,
+    renderFractionalFeeFormFields,
+    renderFixedFeeFormFields,
+  ]);
+
   return (
     <>
       <label htmlFor='fees'>
@@ -33,115 +165,7 @@ const NftFormFees = ({ fees }: Props) => {
           )}
         />
       </label>
-      {fees?.map((fee: string) => {
-        switch (fee) {
-          case 'royaltyFee':
-            return (
-              <div className='form__row__fees'>
-                <label htmlFor='null'>Royalty Fee:</label>
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.feeCollectorAccountId`}
-                  type='text'
-                  label='Fee collector account ID'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.fallbackFee`}
-                  type='number'
-                  label='Fallback fee'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.numerator`}
-                  type='number'
-                  label='Numerator'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.denominator`}
-                  type='number'
-                  label='Denominator'
-                />
-              </div>
-            );
-          case 'fractionalFee':
-            return (
-              <div className='form__row__fees'>
-                <label htmlFor='null'>Fractional Fee:</label>
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.feeCollectorAccountId`}
-                  type='text'
-                  label='Fee collector account ID'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.numerator`}
-                  type='number'
-                  label='Numerator'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.denominator`}
-                  type='number'
-                  label='Denominator'
-                />
-                <FieldWrapper
-                  name={`activeFees.${ fee }.min`}
-                  type='number'
-                  label='Min'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.max`}
-                  type='number'
-                  label='Max'
-                />
-
-                <Switch
-                  name={`activeFees.${ fee }.assessmentMethod`}
-                  options={[
-                    { name: 'Inclusive', value: 'inclusive' },
-                    { name: 'Exclusive', value: 'exclusive' },
-                  ]}
-                />
-              </div>
-            );
-          case 'fixedFee':
-            return (
-              <div className='form__row__fees'>
-                <label htmlFor='null'>Fixed Fee:</label>
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.feeCollectorAccountId`}
-                  type='text'
-                  label='Fee collector account ID'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.denominatingTokenId`}
-                  type='text'
-                  label='Denominating token ID'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.amount`}
-                  type='number'
-                  label='Amount'
-                />
-
-                <FieldWrapper
-                  name={`activeFees.${ fee }.hbarAmount`}
-                  type='number'
-                  label='HBar amount'
-                />
-              </div>
-            );
-          default:
-            return;
-        }
-      })}
+      {renderCheckedFeesFormFields()}
     </>
   );
 };
