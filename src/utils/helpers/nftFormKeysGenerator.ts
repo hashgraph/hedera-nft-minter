c7d325462c321cdb753c95614c8de3a3c9959bce
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2022-05-13 14:51:36 +0200

    Refactor of nftFormKeysGenerator

--- src/utils/helpers/nftFormKeysGenerator.ts
+++ src/utils/helpers/nftFormKeysGenerator.ts
@@ -2,40 +2,50 @@ import { PublicKey } from '@hashgraph/sdk';
 import { FormikValues } from 'formik';
 import _ from 'lodash';
 
-const generateKey = (
-  keys: FormikValues,
-  key: string,
-  value: string,
-  generatedKeyFromAccount: PublicKey | false = false
-) => {
-  switch (value) {
-    case 'custom':
-      return PublicKey.fromString(keys[`${ key }_key`]);
-    case 'account':
-      return generatedKeyFromAccount;
-    default:
-      return;
-  }
+type TokenKeysType = {
+  adminKey?: PublicKey;
+  freezeKey?: PublicKey;
+  kycKey?: PublicKey;
+  supplyKey?: PublicKey;
+  wipeKey?: PublicKey;
+  pauseKey?: PublicKey;
 };
 
+class TokenKeys {
+  _keysNames = ['admin', 'freeze', 'kyc', 'supply', 'wipe', 'pause'];
+  keys;
+  keysTypes;
+  generatedKeyFromAccount;
+
+  constructor(values: FormikValues, userKey: string) {
+    this.generatedKeyFromAccount = PublicKey.fromString(userKey);
+
+    this.keys = _.pickBy(values, (_: string, key: string) => {
+      const keys = ['admin', 'freeze', 'kyc', 'supply', 'wipe', 'pause'];
+      return keys.filter((k) => key.includes(k)).length > 0;
+    }) as FormikValues;
+
+    this.keysTypes = _.pickBy(this.keys, (_, key) => !key.includes('_key'));
+  }
+
+  generateKey(key: string, value: string) {
+    switch (value) {
+      case 'custom':
+        return PublicKey.fromString(this.keys[`${ key }_key`]);
+      case 'account':
+        return this.generatedKeyFromAccount;
+      default:
+        return;
+    }
+  }
+}
+
 export const nftFormKeysGenerator = (values: FormikValues, userKey: string) => {
-  const keys = _.pickBy(values, (_: string, key: string) => {
-    const keys = ['admin', 'freeze', 'kyc', 'supply', 'wipe', 'pause'];
-    return keys.filter((k) => key.includes(k)).length > 0;
-  }) as FormikValues;
+  const tokenKeys = new TokenKeys(values, userKey);
 
-  const keysTypes = _.pickBy(keys, (_, key) => !key.includes('_key'));
-
-  const generatedKeyFromAccount = PublicKey.fromString(userKey);
-
-  const newKeys = {};
-  for (const [key, value] of Object.entries(keysTypes)) {
-    const generatedKey = generateKey(
-      keys,
-      key,
-      value,
-      value === 'account' ? generatedKeyFromAccount : false
-    );
+  const newKeys = {} as TokenKeysType;
+  for (const [key, value] of Object.entries(tokenKeys.keysTypes)) {
+    const generatedKey = tokenKeys.generateKey(key, value);
     if (generatedKey) {
       newKeys[`${ key }Key`] = generatedKey;
     }
