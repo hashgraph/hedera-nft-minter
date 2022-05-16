eb3b4ea4aca419950acbc253565fb6cf3e16f767
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2022-05-16 23:06:28 +0200

    Decrease transition speed

--- src/utils/hooks/useScrollDir.tsx
+++ src/utils/hooks/useScrollDir.tsx
@@ -6,7 +6,7 @@ export enum ScrollDirection {
 }
 
 export const useScrollDirection = () => {
-  const threshold = 80;
+  const threshold = 7;
   const [scrollDir, setScrollDir] = useState(ScrollDirection.up);
 
   useEffect(() => {
