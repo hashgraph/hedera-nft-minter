3a2433aceafe25ceaf3e17ca9699fde2964c531f
Patryk Matyjasiak <patryk.matyjasiak@silksh.pl>
2022-05-26 09:48:11 +0200

    fix transform treasury account id

--- src/utils/helpers/transformToKeys.ts
+++ src/utils/helpers/transformToKeys.ts
@@ -1,12 +1,17 @@
-import { PublicKey, Key } from '@hashgraph/sdk';
-import { TokenKey } from '@utils/entity/TokenKeys';
+import { AccountId, Key, PublicKey } from '@hashgraph/sdk';
+import { TOKEN_KEY, TokenKey } from '@utils/entity/TokenKeys';
 
 interface Keys {
-  [key: string]: Key,
+  [key: string]: Key | AccountId,
 }
 
 export default function transformToKeys(keys: TokenKey[], accountKey: string) {
   return keys.reduce<Keys>((keysObject, key) => {
+    if (key.type === TOKEN_KEY.TREASURY) {
+      keysObject[key.type] = PublicKey.fromString(accountKey).toAccountId(0, 0);
+      return keysObject;
+    }
+
     keysObject[key.type] = PublicKey.fromString(key.value === 'account' ? accountKey : key.key);
     return keysObject;
   }, {});
