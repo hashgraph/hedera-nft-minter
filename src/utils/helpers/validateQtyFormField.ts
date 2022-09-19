b9e16a8e32369c3ef2d56c6718fb03c4abc4d44b
norbert.kulus <norbert.kulus@arianelabs.com>
2022-09-19 14:35:26 +0200

    Not display undefined when not maxSupply is defined

--- src/utils/helpers/validateQtyFormField.ts
+++ src/utils/helpers/validateQtyFormField.ts
@@ -1,7 +1,7 @@
 import { NumberSchema } from 'yup';
 
 export default function validateQtyFormField(max: number, schema: NumberSchema) {
-  if (max >= 10 || max <= 0) {
+  if ((max >= 10 || max <= 0) || !max) {
     return schema.max(10, 'Max 10');
   } else {
     return schema.max(max, `Max ${ max }` )
