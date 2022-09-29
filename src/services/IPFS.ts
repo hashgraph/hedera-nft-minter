8c921271c166d3f2b4f53f80fb9a07f7fc2b820c
norbert.kulus <norbert.kulus@arianelabs.com>
2022-09-29 13:58:13 +0200

    Shuffle api key if uploading request wasnt successfull

--- src/services/IPFS.ts
+++ src/services/IPFS.ts
@@ -27,22 +27,36 @@ export default class IPFS {
   static readonly UPLOAD_URL = '/upload';
   static readonly instance = axios.create({
     baseURL: IPFS_URL,
-    headers: {
-      Authorization: `Bearer ${ IPFS_KEYS[random(0, IPFS_KEYS.length)] }`,
-    }
   });
 
   static async uploadFile(file: File | Blob) {
-    return this.instance.post<UploadRespone>(this.UPLOAD_URL, file, {
-      headers: {
-        'Content-Type': 'image/*'
-      }
-    });
+    let uploadDataResponse;
+
+    while (!uploadDataResponse || uploadDataResponse.status !== 200) {
+      uploadDataResponse = await this.instance.post<UploadRespone>(this.UPLOAD_URL, file, {
+        headers: {
+          'Content-Type': 'image/*',
+          Authorization: `Bearer ${ IPFS_KEYS[random(0, IPFS_KEYS.length)] }`,
+        }
+      });
+    }
+
+    return uploadDataResponse
   }
 
   static async createMetadataFile(meta: NFTMetadata) {
-    const file = new File([JSON.stringify(meta)], 'meta.json', { type: 'application/json' });
+    let uploadDataResponse;
 
-    return this.instance.post<UploadRespone>(this.UPLOAD_URL, file);
+    while (!uploadDataResponse || uploadDataResponse.status !== 200) {
+      const file = new File([JSON.stringify(meta)], 'meta.json', { type: 'application/json' });
+
+      uploadDataResponse = await this.instance.post<UploadRespone>(this.UPLOAD_URL, file, {
+        headers: {
+          Authorization: `Bearer ${ IPFS_KEYS[random(0, IPFS_KEYS.length)] }`,
+        }
+      });
+    }
+
+    return uploadDataResponse
   }
 }
