d6d4009b55b5c1062117bbf465b119698f389d20
norbert.kulus <norbert.kulus@arianelabs.com>
2022-10-03 11:17:26 +0200

    Remove settings from minter

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,10 +5,3 @@ export { default as MyWallet } from './my-nft-collection';
 export { default as Profile } from './profile';
 export { default as Book } from './book';
 export { default as NFTOverview } from './nft-overview';
-
-export { default as SettingsWrapper } from './settings/Wrapper'
-export { default as SettingsProfile } from './settings/profile'
-export { default as SettingsNotifications } from './settings/notifications';
-export { default as SettingsFeaturedItems } from './settings/featured-items';
-export { default as SettingsAccountSupport } from './settings/account-support';
-export { default as SettingsOffers } from './settings/offers';
