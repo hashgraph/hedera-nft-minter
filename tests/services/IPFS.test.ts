d12ee847eaa85f2fb011bd17ca27cb56f13cb65e
Patryk Matyjasiak <patryk.matyjasiak@arianelabs.com>
2022-10-14 13:55:09 +0200

    Update IPFS.test.ts

--- tests/services/IPFS.test.ts
+++ tests/services/IPFS.test.ts
@@ -6,26 +6,61 @@ import { NFTMetadata } from '@utils/entity/NFT-Metadata';
 
 jest.mock('@services/IPFS');
 
+const exampleFile = new File(['foo'], 'foo.txt', { type: 'text/plain'});
+const exampleFile2 = new File(['foo'], 'foo2.txt', { type: 'text/plain'});
+
+const correctResponse = {
+  data: {
+    ok: true,
+    value: {
+      cid: 'test',
+      created: new Date().toDateString(),
+      name: 'test',
+      scope: 'test',
+      size: 10,
+      type: 'file'
+    }
+  },
+  headers: null,
+  status: 200,
+  statusText: 'done',
+  config: {}
+}
+
 describe('Test IPFS service', () => {
   beforeEach(() => {
-    (IPFS as unknown as jest.Mock).mockImplementation(() => ({
-      uploadFile: async (file: File) => {
-        console.log({ file });
-      },
-      createMetadataFile: async (data: NFTMetadata) => {
-        console.log({ data });
-      },
-    }));
+    IPFS.uploadFile = (file: File) => new Promise((resolve) => {
+      if (file.name === 'foo2.txt') { throw new Error('INCORRECT_FILE')}
+
+      resolve(correctResponse)
+    });
+
+    IPFS.createMetadataFile = (meta: NFTMetadata) => new Promise((resolve) => {
+      if (meta.name === 'fail') { throw new Error('INCORRECT_METADATA')}
+
+      resolve(correctResponse)
+    });
   })
 
   test('upload file', async () => {
-    const metadata: NFTMetadata = {
-      name: 'test',
-      creator: 'tester',
-    };
+    expect.assertions(1);
+    try {
+      const res = await IPFS.uploadFile(exampleFile);
+      expect(res.data.ok).toBe(true)
+    } catch (e) {
+      expect(e.message).toBe('INCORRECT_FILE')
+    }
+  });
 
-    expect(IPFS.createMetadataFile(metadata))
+  test('upload file - thrown', async () => {
+    expect.assertions(1);
+    try {
+      const res = await IPFS.uploadFile(exampleFile2);
 
+      expect(res.data.ok).toBe(true)
+    } catch (e) {
+      expect(e.message).toBe('INCORRECT_FILE')
+    }
   });
 
   test('create metadata', async () => {
@@ -34,7 +69,30 @@ describe('Test IPFS service', () => {
       creator: 'tester',
     };
 
-    expect(IPFS.createMetadataFile(metadata))
+    expect.assertions(1);
+    try {
+      const res = await IPFS.createMetadataFile(metadata);
+
+      expect(res.data.ok).toBe(true)
+    } catch (e) {
+      expect(e.message).toBe('INCORRECT_METADATA')
+    }
+  });
+
+  test('create metadata - throw', async () => {
+    const metadata: NFTMetadata = {
+      name: 'fail',
+      creator: 'tester',
+    };
+
+    expect.assertions(1);
+    try {
+      const res = await IPFS.createMetadataFile(metadata);
+
+      expect(res.data.ok).toBe(true)
+    } catch (e) {
+      expect(e.message).toBe('INCORRECT_METADATA')
+    }
 
   });
 });
