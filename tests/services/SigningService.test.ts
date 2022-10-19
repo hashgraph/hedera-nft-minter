4a8ec10c1902e732467e3d457ecee0945798cb7a
Patryk Matyjasiak <patryk.matyjasiak@arianelabs.com>
2022-10-19 13:03:18 +0200

    add test Signing service, helper

--- tests/services/SigningService.test.ts
+++ tests/services/SigningService.test.ts
@@ -4,22 +4,23 @@ import { SigningService } from '@services/SigningService';
 import { AccountCreateTransaction, AccountId, PrivateKey } from '@hashgraph/sdk'
 
 describe('Test Signing service', () => {
+  const pkString = '704085272b147ecc6cf8eed094016555e7eaf68eb4145f0473746053adaecf00';
 
   test('signAndMakeBytes', async () => {
-    const pk = PrivateKey.generateECDSA()
+    const pk = PrivateKey.fromString(pkString);
     const tx = new AccountCreateTransaction()
       .setKey(pk)
       .setNodeAccountIds([new AccountId(3)])
     ;
 
     const bytes = SigningService.signAndMakeBytes(tx, '0.0.123456');
-    const correct = Buffer.from([10, 124, 42, 122, 10]);
+    const correct = Buffer.from([10, 123, 42, 121, 10]);
 
     expect(bytes.slice(0, 5)).toEqual(correct)
   });
 
   test('makeBytes', async () => {
-    const pk = PrivateKey.generateECDSA()
+    const pk = PrivateKey.fromString(pkString);
     const tx = new AccountCreateTransaction()
       .setKey(pk)
     ;
---
+++ tests/utils/helpers/filterFormValuesToNFTMetadata.test.ts
@@ -0,0 +1,32 @@
+import { describe, it, expect } from '@jest/globals';
+import filterFormValuesToNFTMetadata from '@utils/helpers/filterFormValuesToNFTMetadata';
+
+describe('Filter form values to metadata', () => {
+  it('empty values', () => {
+    expect(filterFormValuesToNFTMetadata({})).toEqual({format: 'HIP412@1.0.0'});
+  });
+
+  it('correct metadata', () => {
+    const values = {
+      edition_name: 'Test',
+      description: 'lorem ipsum',
+      type: 'image/jpeg',
+      image: 'ipfs://bafkreiahrxk5xvmuqn2jmpaj2oemm777fazloc43ltxnsivzwigxy4cyjm',
+      creator: 'Tester',
+      attributes: [{ trait_type: 'height', value: 10 }],
+      properties: [{ label: 'foo', value: 'bar'}]
+    };
+
+    expect(filterFormValuesToNFTMetadata({...values, files: ''}))
+      .toEqual({
+        name: 'Test',
+        description: 'lorem ipsum',
+        type: 'image/jpeg',
+        image: 'ipfs://bafkreiahrxk5xvmuqn2jmpaj2oemm777fazloc43ltxnsivzwigxy4cyjm',
+        creator: 'Tester',
+        format: 'HIP412@1.0.0',
+        attributes: [{ trait_type: 'height', value: 10 }],
+        properties: [{ label: 'foo', value: 'bar'}],
+      });
+  })
+});
