d6c64658ec26a26cca064bf18580ba01824d0ebc
Patryk Matyjasiak <patryk.matyjasiak@arianelabs.com>
2022-10-20 16:11:00 +0200

    Update HTS.test.ts

--- tests/services/HTS.test.ts
+++ tests/services/HTS.test.ts
@@ -1,16 +1,95 @@
+/** @jest-environment jsdom */
+
 import { describe, test, expect, jest, beforeEach } from '@jest/globals';
-import { AccountId, Hbar, TokenMintTransaction, TokenUpdateTransaction, TransactionId, TransferTransaction } from '@hashgraph/sdk'
-import HTS from '@services/HTS';
+import { AccountId, Hbar, TokenCreateTransaction, TokenMintTransaction, TokenType, TokenSupplyType, TokenUpdateTransaction, TransactionId, TransferTransaction } from '@hashgraph/sdk'
+import HTS, { NewTokenType } from '@services/HTS';
 
 describe('Test HTS service', () => {
+  const now = Date.now();
+
+  Date.now = () => now;
 
   beforeEach(() => {
     const mockFn = jest.fn().mockReturnValue(TransactionId.fromString('0.0.123456@1666180043.380955673'));
 
     TransactionId.generate = mockFn as () => TransactionId;
-  })
 
-  // '0.0.123456@1666180043.380955673'
+  });
+
+  // eslint-disable-next-line no-undef
+  const overrideFetch = (data: unknown) => global.fetch = jest.fn(() =>
+    Promise.resolve({
+      headers: new Headers(),
+      ok: true,
+      redirected: false,
+      status: 200,
+      statusText: 'OK',
+      trailer: new Promise<Headers>((r) => r(new Headers())),
+      // eslint-disable-next-line no-undef
+      type: 'basic' as ResponseType,
+      url: 'url',
+      body: null,
+      bodyUsed: true,
+      clone: () => new Response(),
+      arrayBuffer: () => Promise.resolve(new ArrayBuffer(1)),
+      formData: () => Promise.resolve(new FormData()),
+      blob: () => Promise.resolve(new Blob()),
+      text: () => Promise.resolve('test'),
+      json: () => Promise.resolve(data),
+    })
+  );
+
+  test('createToken - success', async () => {
+    const accountInfo = {
+      result: 'ok',
+      key: {
+        key: '123456'
+      }
+    };
+
+    overrideFetch(accountInfo);
+
+    const tokenProps: NewTokenType = {
+      accountId: '0.0.123456',
+      tokenName: 'Test',
+      tokenSymbol: 'TT',
+      amount: 10,
+    }
+    const mintTx = await HTS.createToken(tokenProps);
+
+    expect(mintTx).toEqual(
+      new TokenCreateTransaction({
+        tokenType: TokenType.NonFungibleUnique,
+        supplyType: TokenSupplyType.Finite,
+        decimals: 0,
+        expirationTime: new Date(now + 3600 * 24 * 12),
+        ...tokenProps,
+        customFees: [],
+      })
+        .setMaxTransactionFee(50)
+    );
+  });
+
+  test('createToken - failed', async () => {
+    // eslint-disable-next-line no-undef
+    overrideFetch({});
+
+    const tokenProps: NewTokenType = {
+      accountId: '0.0.123456',
+      tokenName: 'Test',
+      tokenSymbol: 'TT',
+      amount: 10,
+    }
+
+    expect.assertions(1);
+    try {
+      await HTS.createToken(tokenProps);
+    } catch (e) {
+      expect(e.message).toBe('Error while try to fetch user Public key.')
+    }
+
+  });
+
   test('mintToken', async () => {
     const mintTx = HTS.mintToken('0.0.123456', '0.0.987654', ['bafkreiahrxk5xvmuqn2jmpaj2oemm777fazloc43ltxnsivzwigxy4cyjm']);
 
