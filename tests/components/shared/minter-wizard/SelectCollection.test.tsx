418607b49595a1fbddd0a99ce0296c19b88434fc
Patryk Matyjasiak <patryk.matyjasiak@arianelabs.com>
2022-11-03 10:10:06 +0100

    Update SelectCollection.test.tsx

--- tests/components/shared/minter-wizard/SelectCollection.test.tsx
+++ tests/components/shared/minter-wizard/SelectCollection.test.tsx
@@ -27,12 +27,13 @@ import SelectCollection from '@components/shared/minter-wizard/SelectCollection'
 import { FormWizardSteps, MinterWizardContext } from '@components/views/minter-wizard';
 import HederaWalletsProvider from '@utils/context/HederaWalletsContext';
 import useHederaWallets from '@utils/hooks/useHederaWallets';
+import { TokenSupplyType } from '@utils/entity/TokenInfo';
 
 jest.mock('@utils/hooks/useHederaWallets');
 
 describe('SelectCollection', () => {
   it('render', () => {
-    (useHederaWallets as jest.Mock).mockReturnValue({ userWalletId: '0.0.123456'})
+    (useHederaWallets as jest.Mock).mockReturnValue({ userWalletId: '0.0.2661933'})
     const fn = jest.fn()
     const submit = jest.fn(v => v)
 
@@ -48,6 +49,75 @@ describe('SelectCollection', () => {
             setShowWarning: fn,
             setCollections: fn,
             collections: [],
+          }}
+        >
+          <Formik initialValues={{}} onSubmit={submit}>
+            <SelectCollection />
+          </Formik>
+        </MinterWizardContext.Provider>
+      </HederaWalletsProvider>
+    );
+  })
+
+  it('render - empty userWalletId', () => {
+    (useHederaWallets as jest.Mock).mockReturnValue({ userWalletId: null })
+    const fn = jest.fn()
+    const submit = jest.fn(v => v)
+
+    render(
+      <HederaWalletsProvider>
+        <MinterWizardContext.Provider
+          value={{
+            creatorStep: FormWizardSteps.WelcomeScreen,
+            showWarning: false,
+            creatorStepToBackFromSummary: 0,
+            setCreatorStepToBackFromSummary: fn,
+            setCreatorStepToBackFromSummaryToCurrent: fn,
+            setShowWarning: fn,
+            setCollections: fn,
+            collections: [
+              {
+                nfts: [
+                  {
+                    account_id: '0.0.123456',
+                    created_timestamp: '11213123.2212',
+                    deleted: false,
+                    metadata: '12903812903812093',
+                    modified_timestamp: '123123.21321312',
+                    serial_number: 0,
+                    token_id: '0.0.654321',
+                    meta: { name: 'test' },
+                    spender: null,
+                  }
+                ],
+                info: {
+                  admin_key: null,
+                  auto_renew_account: null,
+                  auto_renew_period: null,
+                  created_timestamp: '1660719957.665494003',
+                  decimals: 0,
+                  deleted: false,
+                  expiry_timestamp: '1660720991592000000',
+                  fee_schedule_key: null,
+                  freeze_default: false,
+                  freeze_key: null,
+                  initial_supply: '0',
+                  kyc_key: null,
+                  max_supply: '10',
+                  name: 'AVATAR',
+                  pause_key: null,
+                  pause_status: 'NOT_APPLICABLE',
+                  supply_key: { _type: 'ED25519', key: '3d9639fcb8137a17c0e0aba20a7fb5038f74269835ef32d3891415008e4db8d2' },
+                  supply_type: TokenSupplyType.FINITE,
+                  symbol: 'PM-AV',
+                  token_id: '0.0.47909758',
+                  total_supply: '4',
+                  treasury_account_id: '0.0.2661933',
+                  type: 'NON_FUNGIBLE_UNIQUE',
+                  wipe_key: null
+                }
+              }
+            ],
           }}
         >
           <Formik initialValues={{}} onSubmit={submit}>
