c817c4c9cc9ac135d4a51b8345ef139ccbaa2d62
Norbert Kulus <norbert.kulus@arianelabs.com>
2023-02-08 11:35:07 +0100

    Add multiple IPFS gateways logic
    Signed-off-by: Norbert Kulus <norbert.kulus@arianelabs.com>

--- Global.d.ts
+++ Global.d.ts
@@ -24,11 +24,11 @@ declare const APP_NAME: string;
 declare const HEDERA_NETWORK: 'testnet' | 'mainnet';
 declare const HEDERA_MIRROR_NODE_API_VERSION: string;
 declare const IPFS_URL: string;
-declare const IPFS_KEYS: string;
 declare const API_HOST: string;
 declare const HASHPACK_APP_CONFIG_NAME: string;
 declare const HASHPACK_APP_CONFIG_DESCRIPTION: string;
 declare const HASHPACK_APP_CONFIG_ICON_URL: string;
+declare const IPFS_GATEWAYS: string[] | undefined;
 
 // eslint-disable-next-line no-underscore-dangle
 const _APP_NAME = APP_NAME;
@@ -39,8 +39,6 @@ const _HEDERA_MIRROR_NODE_API_VERSION = HEDERA_MIRROR_NODE_API_VERSION;
 // eslint-disable-next-line no-underscore-dangle
 const _IPFS_URL = IPFS_URL;
 // eslint-disable-next-line no-underscore-dangle
-const _IPFS_KEYS = IPFS_KEYS;
-// eslint-disable-next-line no-underscore-dangle
 const _API_HOST = API_HOST;
 // eslint-disable-next-line no-underscore-dangle
 const _HASHPACK_APP_CONFIG_NAME = HASHPACK_APP_CONFIG_NAME;
@@ -48,17 +46,19 @@ const _HASHPACK_APP_CONFIG_NAME = HASHPACK_APP_CONFIG_NAME;
 const _HASHPACK_APP_CONFIG_DESCRIPTION = HASHPACK_APP_CONFIG_DESCRIPTION;
 // eslint-disable-next-line no-underscore-dangle
 const _HASHPACK_APP_CONFIG_ICON_URL = HASHPACK_APP_CONFIG_ICON_URL;
+// eslint-disable-next-line no-underscore-dangle
+const _IPFS_GATEWAYS = IPFS_GATEWAYS;
 
 export {
   _APP_NAME as APP_NAME,
   _HEDERA_NETWORK as HEDERA_NETWORK,
   _HEDERA_MIRROR_NODE_API_VERSION as HEDERA_MIRROR_NODE_API_VERSION,
   _IPFS_URL as IPFS_URL,
-  _IPFS_KEYS as IPFS_KEYS,
   _API_HOST as API_HOST,
   _HASHPACK_APP_CONFIG_NAME as HASHPACK_APP_CONFIG_NAME,
   _HASHPACK_APP_CONFIG_DESCRIPTION as HASHPACK_APP_CONFIG_DESCRIPTION,
-  _HASHPACK_APP_CONFIG_ICON_URL as HASHPACK_APP_CONFIG_ICON_URL
+  _HASHPACK_APP_CONFIG_ICON_URL as HASHPACK_APP_CONFIG_ICON_URL,
+  _IPFS_GATEWAYS as IPFS_GATEWAYS
 }
 
 declare module '*.svg' {
--- src/services/IPFS.ts
+++ src/services/IPFS.ts
@@ -17,9 +17,9 @@
  *
  */
 
-import axios from 'axios';
-import random from 'lodash/random';
-import { IPFS_URL, IPFS_KEYS } from '@src/../Global.d';
+import axios, { AxiosResponse } from 'axios';
+import { Buffer } from 'buffer';
+import { IPFS_GATEWAYS, IPFS_URL } from '@src/../Global.d';
 import { NFTMetadata } from '@utils/entity/NFT-Metadata';
 
 export interface UploadResponse {
@@ -42,21 +42,18 @@ export interface UploadResponse {
   };
 }
 
+const DEFAULT_IPFS_PROVIDER = 'https://ipfs.io/ipfs/{CID}'
+
 export default class IPFS {
   static readonly UPLOAD_URL = '/upload';
   static readonly instance = axios.create({
     baseURL: IPFS_URL,
   });
+  static gateways = IPFS_GATEWAYS ?? [DEFAULT_IPFS_PROVIDER]
 
   static async uploadFile(file: File | Blob) {
     try {
-      return await this.instance.post<UploadResponse>(this.UPLOAD_URL, file, {
-        headers: {
-          'Content-Type': 'image/*',
-          Authorization: `Bearer ${ IPFS_KEYS[random(0, IPFS_KEYS.length - 1)] }`,
-        }
-      });
-
+      return await this.instance.post<UploadResponse>(this.UPLOAD_URL, file);
     } catch (e) {
       throw new Error('We are experiencing very high demand. Please retry in 2 minutes.')
     }
@@ -66,13 +63,76 @@ export default class IPFS {
     try {
       const file = new File([JSON.stringify(meta)], 'meta.json', { type: 'application/json' });
 
-      return await this.instance.post<UploadResponse>(this.UPLOAD_URL, file, {
-        headers: {
-          Authorization: `Bearer ${ IPFS_KEYS[random(0, IPFS_KEYS.length - 1)] }`,
-        }
-      });
+      return await this.instance.post<UploadResponse>(this.UPLOAD_URL, file);
     } catch (e) {
       throw new Error('We are experiencing very high demand. Please retry in 2 minutes.')
     }
+  }
+
+  static async fetchDataFromCid(cid: string): Promise<NFTMetadata> {
+    let counter = 0
+
+    do {
+      const url = this.gateways[counter].replace('{CID}', cid)
+      let res: AxiosResponse<NFTMetadata> | null = null
+      
+      try {
+        res = await IPFS.instance.get(url)
+      } catch {
+        counter = counter + 1
+        continue
+      }
+
+      if (res && res.status === 200) {
+        return res.data
+      }
+
+    } while (counter < this.gateways.length)
+
+    throw new Error('Fetch metadata failed')
+  }
+
+  static async fetchData(metadata: string) {
+    if (/^0*$/.test(Buffer.from(metadata).toString('hex'))) {
+      return null
+    }
+
+    if (metadata.includes('https://')) {
+      const { data } = await this.instance.get(metadata)
+
+      return data
+    }
+    
+    return await this.fetchDataFromCid(atob(metadata).replace('ipfs://', ''))
+  }
+  
+  static async fetchImage(cidImage: string) {
+    let counter = 0
+
+    do {
+      const url = this.gateways[counter].replace('{CID}', cidImage)
+      let res: AxiosResponse | null = null
+      
+      try {
+        res = await IPFS.instance.get(url, {
+          responseType: 'arraybuffer',
+        })
+      } catch {
+        counter = counter + 1
+        continue
+      }
+
+
+      if (res && res.status === 200) {
+        const { data, headers } = res
+        const base64 = Buffer.from(data, 'binary').toString('base64')
+        const src = `data:${ headers['content-type'] };base64,${ base64 }`
+
+        return src
+      }
+
+    } while (counter < this.gateways.length)
+
+    throw new Error('Fetch image failed')
   }
 }
