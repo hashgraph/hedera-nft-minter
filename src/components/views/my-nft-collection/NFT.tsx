5693e9611814c351538c9eb0d50ada43a1ee2007
norbert-kulus-arianelabs <102658314+norbert-kulus-arianelabs@users.noreply.github.com>
2023-02-15 10:27:17 +0100

    Add multiple ipfs gateways logic (#26)
    Signed-off-by: Norbert Kulus <norbert.kulus@arianelabs.com>
    Signed-off-by: norbert-kulus-arianelabs <102658314+norbert-kulus-arianelabs@users.noreply.github.com>

--- src/components/views/my-nft-collection/NFT.tsx
+++ src/components/views/my-nft-collection/NFT.tsx
@@ -20,16 +20,16 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import classNames from 'classnames';
 
+import IPFS from '@src/services/IPFS';
 import { NFTMetadata } from '@utils/entity/NFT-Metadata';
-import MirrorNode from '@services/MirrorNode';
 import { TokenInfo } from '@utils/entity/TokenInfo';
 import renderValue from '@utils/helpers/renderValue';
 import { NFTInfo } from '@utils/entity/NFTInfo';
 import { HEDERA_NETWORK } from '@src/../Global.d';
-import formatToIPFSImageLink from '@utils/helpers/formatToIPFSImageLink';
 import HIP412MetadataSchema from '@src/utils/const/HIP412MetadataSchema';
 
 import Loader from '@components/shared/loader/Loader';
+import { IPFSImage } from '@src/components/shared/IPFSImage';
 
 import placeholder from '@assets/images/placeholder.png';
 import './nft.scss';
@@ -38,36 +38,51 @@ type NFTProps = NFTInfo & {
   collectionInfo?: TokenInfo
 }
 
+enum CardMessages {
+  VALIDATION_WARNING = 'Not HIP-412 compliant',
+  FETCHING_ERROR = 'Metadata loading error'
+}
+
 export default function NFT({ metadata, serial_number, collectionInfo }: NFTProps) {
   const [loadedMetadata, setLoadedMetadata] = useState<NFTMetadata | null>(null);
-  const [hasHip412ComplianMetadata, setHip412ComplianMetadata] = useState(true)
+  const [message, setMessage] = useState<CardMessages | null>(null)
   const [isLoading, setIsLoading] = useState(true);
 
   const loadMetadata = useCallback(async () => {
-    const fetchedMetadata = await MirrorNode.fetchNFTMetadata(atob(metadata));
-    const metadataValidationResult = HIP412MetadataSchema.isValidSync(fetchedMetadata)
+    if (!metadata) {
+      setIsLoading(false)
+      return
+    }
 
-    setLoadedMetadata(fetchedMetadata);
+    try {
+      const fetchedMetadata = await IPFS.fetchData(metadata)
 
-    setHip412ComplianMetadata(metadataValidationResult)
+      if (fetchedMetadata) {
+        setLoadedMetadata(fetchedMetadata);
+      }
+      
+      const metadataValidationResult = HIP412MetadataSchema.isValidSync(fetchedMetadata)
+      
+      if (!metadataValidationResult) {
+        setMessage(CardMessages.VALIDATION_WARNING)
+      }
+    } catch {
+      setMessage(CardMessages.FETCHING_ERROR)
+    }
+
     setIsLoading(false);
-  }, [metadata, setLoadedMetadata]);
-
-  const previewImageSrc = useMemo(() => (
-    typeof loadedMetadata?.image === 'string' && loadedMetadata.image.length > 0 ? (
-      formatToIPFSImageLink(loadedMetadata.image)
-    ) : (
-      placeholder
-    )
-  ), [loadedMetadata?.image])
+  }, [metadata]);
 
   const nftCardClassnames = useMemo(() => (
     classNames('nft-card', { nft__isLoading: isLoading })
   ), [isLoading])
 
-  const nftCardImageClassnames = useMemo(() => (
-    classNames({placeholder: !loadedMetadata?.image})
-  ), [loadedMetadata?.image])
+  const nftCardMessageClassNamees = useMemo(() => (
+    classNames({
+      'nft-card__message--warning': message === CardMessages.VALIDATION_WARNING,
+      'nft-card__message--error': message === CardMessages.FETCHING_ERROR,
+    })
+  ), [message])
 
   useEffect(() => {
     loadMetadata();
@@ -86,14 +101,22 @@ export default function NFT({ metadata, serial_number, collectionInfo }: NFTProp
       ) : (
         <>
           <div className='nft-card__image'>
-            {!hasHip412ComplianMetadata && (
-              <p className='nft-card__warning'>Not HIP-412 compliant</p>
+            {message && (
+              <p className={nftCardMessageClassNamees}>{message}</p>
             )}
-            <img
-              className={nftCardImageClassnames}
-              src={previewImageSrc}
-              alt='nft_image'
-            />
+
+            {loadedMetadata && loadedMetadata.image ? (
+              <IPFSImage
+                cidOrUrlToImage={loadedMetadata.image}
+                alt='nft_image_preview'
+              />
+            ) : (
+              <img
+                className='placeholder'
+                src={placeholder}
+                alt='placeholder'
+              />
+            )}
           </div>
 
           <div className='nft-card__content'>
